e0b2bd586660c9e33778689d1de5dfdcb14870ee
palashbairagi <palashbairagi11122@gmail.com>
2019-09-04 10:48:02 -0400

    Initial Commit

---
+++ src/test/java/com/exchange/buysell/BuysellApplicationTests.java
@@ -0,0 +1,16 @@
+package com.exchange.buysell;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BuysellApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
