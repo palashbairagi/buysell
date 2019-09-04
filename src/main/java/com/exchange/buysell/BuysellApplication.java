4eed22e0c2ddb97e6b270cda7353a42ff5269f6c
ahsanshahrukh <khan.ahsan053@gmail.com>
2019-09-04 14:52:21 -0400

    Entity Classes Added!!

--- src/main/java/com/exchange/buysell/BuysellApplication.java
+++ src/main/java/com/exchange/buysell/BuysellApplication.java
@@ -1,13 +1,20 @@
 package com.exchange.buysell;
 
+import javafx.application.Application;
+
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class BuysellApplication {
-
+	private static final Logger log = (Logger) LoggerFactory.getLogger(Application.class);
 	public static void main(String[] args) {
 		SpringApplication.run(BuysellApplication.class, args);
+
+		System.out.println("Hello World");
 	}
 
 }
---
+++ src/test/java/com/exchange/buysell/Stocks.java
@@ -0,0 +1,9 @@
+package com.exchange.buysell;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+public class Stocks {
+
+
+}
