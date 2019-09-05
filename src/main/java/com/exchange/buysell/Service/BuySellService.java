aeb1c0b5bd7255d3705425520553ca7978e40ac2
Ravi <Rpatel1291@gmail.com>
2019-09-05 14:32:56 -0400

    Hello World

---
+++ src/main/java/com/exchange/buysell/Service/BuySellService.java
@@ -0,0 +1,9 @@
+package com.exchange.buysell.service;
+
+import org.springframework.stereotype.Service;
+
+public interface BuySellService{
+
+
+
+}
---
+++ src/main/java/com/exchange/buysell/Service/BuySellServieImp.java
@@ -0,0 +1,9 @@
+package com.exchange.buysell.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+class BuySellServiceImp implements BuySellService {
+
+
+}
