a2a6a906401156874b9ab5fc7cfc06cddd1da2cd
Ravi <Rpatel1291@gmail.com>
2019-09-05 11:50:11 -0400

    init

--- src/main/java/com/exchange/buysell/controllers/SearchController.java
+++ src/main/java/com/exchange/buysell/controllers/SearchController.java
@@ -1,17 +1,15 @@
-package com.exchange.buysell.Controllers;
+package com.exchange.buysell.controllers;
 
-import com.exchange.buysell.Entity.Stocks;
-import com.exchange.buysell.Service.SearchService;
-import com.exchange.buysell.Service.SearchServiceImp;
+import com.exchange.buysell.entity.Stocks;
+import com.exchange.buysell.service.SearchService;
+import com.exchange.buysell.service.SearchServiceImp;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("“stockapi/v1/search")
 public class SearchController {
-
 
     @RequestMapping(name = "“stockapi/v1/search/{stockname}?pageNumber=1&offset=10” ", method = RequestMethod.GET)
     public List<Stocks> findItem(@PathVariable(name = "stockname") String stockname) {
