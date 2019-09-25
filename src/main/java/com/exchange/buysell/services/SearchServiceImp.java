7c99f8acec4789c40f9454f68769c3041f664d78
rpatel1291 <rpatel1291@gmail.com>
2019-09-24 20:41:03 -0400

    Update SearchServiceImp.java

--- src/main/java/com/exchange/buysell/services/SearchServiceImp.java
+++ src/main/java/com/exchange/buysell/services/SearchServiceImp.java
@@ -3,24 +3,27 @@ package com.exchange.buysell.services;
 
 import com.exchange.buysell.entity.Data;
 import com.exchange.buysell.entity.MyJSONObject;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
 
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 
 @Service
 public class SearchServiceImp implements SearchInterface {
-    private final String URL = "http://api.worldtradingdata.com/api/v1/stock?symbol=AB&api_token=cYDw6OCIFS7okSkLnHj4oR6kP5H3pbQJNBQ9RIFUKZ6MOZYQxAHHhaBWsTfx";
+    private final String URL = "https://api.worldtradingdata.com/api/v1/stock?symbol=AAPL&api_token=IMVwkyMvqyG1GSVEGbIDjFeRwPmW2rvYzibGVX1NelH7H0SgzOeKF5s3rAbO";
 
     public List<Data> StockBySymbol(String symbol){
         RestTemplate restTemplate = new RestTemplate();
-        MyJSONObject myJSONObject = restTemplate.getForObject(URL, MyJSONObject.class);
 
-        System.out.println(myJSONObject);
-
-        return myJSONObject.getDataList();
+        ResponseEntity<String> responseEntity =  restTemplate.getForEntity(URL, String.class);
+        System.out.println(responseEntity.toString());
+        return null;//(List<Data>) myJSONObject.getDataList();
     }
 
 }
